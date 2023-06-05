4e14ba1e97062b52ce07b5908abb41a4cb1b8534
miked <mikedegroot1999@hotmail.com>
2023-06-05 16:09:13 +0200

    refactor(locale): Fixed the LocaleManager#toServerLocale by properly scanning and loading for the english locale.

--- src/main/java/me/pikamug/localelib/LocaleLib.java
+++ src/main/java/me/pikamug/localelib/LocaleLib.java
@@ -26,159 +26,17 @@ package me.pikamug.localelib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-
 @SuppressWarnings("unused")
 public class LocaleLib extends JavaPlugin {
     private LocaleManager manager;
     
     @Override
     public void onEnable() {
-        print();
-
         manager = new LocaleManager();
     }
 
     @Override
     public void onDisable() {
-    }
-
-    public void print() {
-        listResourceTree("assets/minecraft/lang", "", 0, 3);
-        List<String> matchingPaths = searchResources("assets/minecraft/lang", ".json", 3);
-        System.out.println("Matching paths:");
-        for (String path : matchingPaths) {
-            System.out.println(path);
-        }
-    }
-
-    private void listResourceTree(String folder, String parent, int depth, int maxDepth) {
-        System.out.println(getIndentation(depth) + "+" + parent);
-        if (depth == maxDepth) return;
-        try {
-            List<String> resourcePaths = getResourcePaths(folder);
-            for (String resourcePath : resourcePaths) {
-                if (isResourceDirectory(folder, resourcePath)) {
-                    listResourceTree(folder + "/" + resourcePath, resourcePath, depth + 1, maxDepth);
-                } else {
-                    System.out.println(getIndentation(depth + 1) + "|-" + resourcePath);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<String> getResourcePaths(String folder) throws IOException {
-        List<String> resourcePaths = new ArrayList<>();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> urls = loader.getResources(folder);
-
-        while (urls.hasMoreElements()) {
-            URL url = urls.nextElement();
-            String protocol = url.getProtocol();
-            if (protocol.equals("jar")) {
-                String jarPath = getJarPath(url);
-                if (jarPath != null) {
-                    try (JarFile jarFile = new JarFile(URLDecoder.decode(jarPath, "UTF-8"))) {
-                        resourcePaths.addAll(getResourcePathsFromJar(jarFile, folder));
-                    }
-                }
-            } else if (protocol.equals("file")) {
-                String filePath = URLDecoder.decode(url.getPath(), "UTF-8");
-                try {
-                    resourcePaths.addAll(getResourcePathsFromFile(filePath, folder));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return resourcePaths;
-    }
-
-    private String getJarPath(URL jarUrl) {
-        String path = jarUrl.getPath();
-        int separatorIndex = path.indexOf('!');
-        if (separatorIndex != -1) {
-            return path.substring("file:".length(), separatorIndex);
-        }
-        return null;
-    }
-
-    private List<String> getResourcePathsFromJar(JarFile jarFile, String folder) {
-        List<String> resourcePaths = new ArrayList<>();
-
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String entryName = entry.getName();
-            if (entryName.startsWith(folder + "/")) {
-                String resourcePath = entryName.substring(folder.length() + 1);
-                if (!resourcePath.isEmpty() && !resourcePath.contains("/")) {
-                    resourcePaths.add(resourcePath);
-                }
-            }
-        }
-
-        return resourcePaths;
-    }
-
-    private List<String> getResourcePathsFromFile(String filePath, String folder) throws IOException {
-        List<String> resourcePaths = new ArrayList<>();
-        java.io.File file = new java.io.File(filePath + "/" + folder);
-        if (file.exists() && file.isDirectory()) {
-            String[] children = file.list();
-            if (children != null) {
-                for (String child : children) {
-                    if (!child.contains("/")) {
-                        resourcePaths.add(child);
-                    }
-                }
-            }
-        }
-        return resourcePaths;
-    }
-
-    private boolean isResourceDirectory(String folder, String resourcePath) {
-        return resourcePath.indexOf('/') == -1;
-    }
-
-    private String getIndentation(int depth) {
-        StringBuilder indentation = new StringBuilder();
-        for (int i = 0; i < depth; i++) {
-            indentation.append("  |");
-        }
-        return indentation.toString();
-    }
-
-    private List<String> searchResources(String folder, String searchCriteria, int maxDepth) {
-        List<String> matchingPaths = new ArrayList<>();
-        try {
-            searchResourcesRecursive(folder, "", 0, maxDepth, searchCriteria, matchingPaths);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return matchingPaths;
-    }
-
-    private void searchResourcesRecursive(String folder, String parent, int depth, int maxDepth, String searchCriteria, List<String> matchingPaths) throws IOException {
-        List<String> resourcePaths = getResourcePaths(folder);
-        for (String resourcePath : resourcePaths) {
-            if (resourcePath.endsWith(searchCriteria)) {
-                matchingPaths.add(getIndentation(depth + 1) + "- " + folder + "/" + resourcePath);
-            }
-            if (depth < maxDepth && isResourceDirectory(folder, resourcePath)) {
-                searchResourcesRecursive(folder + "/" + resourcePath, resourcePath, depth + 1, maxDepth, searchCriteria, matchingPaths);
-            }
-        }
     }
 
     public LocaleManager getLocaleManager() {
---
+++ src/main/java/me/pikamug/localelib/SystemResourcesUtil.java
@@ -0,0 +1,80 @@
+package me.pikamug.localelib;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SystemResourcesUtil {
+
+    /**
+     * Returns the context ClassLoader for this thread. The context ClassLoader may be set by the creator of the thread for use by code running in this thread when loading classes and resources. If not {@link Thread#setContextClassLoader(ClassLoader) set}, the default is to inherit the context class loader from the parent thread.
+     * The context ClassLoader of the primordial thread is typically set to the class loader used to load the application.
+     * @return The context class loader of the current thread.
+     */
+    public static ClassLoader getContextClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * Searches for all resource paths found inside the baseFolder on the loader using the searchCriteria.
+     * @param loader The class loader to search on
+     * @param baseFolder The resource folder to search in
+     * @param searchCriteria The search criteria to apply
+     * @return An iterator that iterates over all matching resources
+     * @throws IOException if an I/O error has occurred
+     */
+    public static Iterator<String> findResourcesBySearch(ClassLoader loader, String baseFolder, String searchCriteria) throws IOException {
+        Pattern pattern = Pattern.compile(searchCriteria);
+        Enumeration<URL> urls = loader.getResources(baseFolder);
+
+        List<String> matchingResources = new ArrayList<>();
+
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            String protocol = url.getProtocol();
+            if (protocol.equals("jar")) {
+                String jarPath = getJarPath(url);
+                if (jarPath != null) {
+                    try (JarFile jarFile = new JarFile(URLDecoder.decode(jarPath, "UTF-8"))) {
+                        matchingResources.addAll(getMatchingResourcesFromJar(jarFile, pattern, baseFolder));
+                    }
+                }
+            }
+        }
+
+        return matchingResources.iterator();
+    }
+
+    private static String getJarPath(URL jarUrl) {
+        String path = jarUrl.getPath();
+        int separatorIndex = path.indexOf('!');
+        if (separatorIndex != -1) {
+            return path.substring("file:".length(), separatorIndex);
+        }
+        return null;
+    }
+
+    private static List<String> getMatchingResourcesFromJar(JarFile jarFile, Pattern pattern, String baseFolder) {
+        List<String> matchingResources = new ArrayList<>();
+
+        Enumeration<JarEntry> entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            String entryName = entry.getName();
+            if (entryName.startsWith(baseFolder)) {
+                String remainingPath = entryName.substring(baseFolder.length());
+                Matcher matcher = pattern.matcher(remainingPath);
+                if (matcher.find()) {
+                    matchingResources.add(entryName);
+                }
+            }
+        }
+
+        return matchingResources;
+    }
+}
